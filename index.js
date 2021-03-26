135f54cc94b5b7c6dca514961aa68e612bf9bbce
nick(work pc) <phpnick.ru@gmail.com>
2021-03-26 10:34:09 +0500

    1

---
+++ index.js
@@ -0,0 +1,391 @@
+var app = new Vue({
+  el: "#app",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+      	{
+          src: "images/1/1.jpg",
+        },      
+      	{
+          src: "images/1/1a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app2",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/1/2.jpg",
+        },      
+        {
+          src: "images/1/2a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app3",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/1/3.jpg",
+        },      
+        {
+          src: "images/1/3a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app4",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/2/1.jpg",
+        },      
+        {
+          src: "images/2/1a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+
+var app = new Vue({
+  el: "#app5",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/2/2.jpg",
+        },      
+        {
+          src: "images/2/2a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app6",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/2/3.jpg",
+        },      
+        {
+          src: "images/2/3a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app7",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/2/4.jpg",
+        },      
+        {
+          src: "images/2/4a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app8",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/3/1.jpg",
+        },      
+        {
+          src: "images/3/1a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+
+var app = new Vue({
+  el: "#app9",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/3/2.jpg",
+        },      
+        {
+          src: "images/3/2a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app10",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/3/3.jpg",
+        },      
+        {
+          src: "images/3/3a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app11",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/3/4.jpg",
+        },      
+        {
+          src: "images/3/4a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app12",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/4/1.jpg",
+        },      
+        {
+          src: "images/4/1a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app13",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/4/2.jpg",
+        },      
+        {
+          src: "images/4/2a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app14",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/4/3.jpg",
+        },      
+        {
+          src: "images/4/3a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
+
+var app = new Vue({
+  el: "#app15",
+  data() {
+    return {
+      image: null,
+      switch: null,
+      images: [
+        {
+          src: "images/4/4.jpg",
+        },      
+        {
+          src: "images/4/4a.jpg",
+        },
+      ]
+    }
+  },
+  mounted() {
+    this.image = this.images[0];
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[1];
+    }
+  }
+});
